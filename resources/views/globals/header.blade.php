6c139093e5f5f8c985592a9b3d2af383881d78c8
tiendatpy <datn71356@gmail.com>
2025-04-07 16:28:19 +0700

    refactor header layout for improved button accessibility

--- resources/views/globals/header.blade.php
+++ resources/views/globals/header.blade.php
@@ -9,18 +9,20 @@
       <div class="header-search down_xl:hidden flex items-center">
         <input type="text" class="h-[45px] rounded-2xl border-1 w-[402px] px-7 py-6 text-sm" placeholder="Search">
       </div>
-      <div class="hamburger-menu flex xl:hidden">
-        <span class="icomoon icon-menu-alt-1 text-h3"></span>
+      <div class="hamburger-menu xl:hidden">
+        <button class="flex">
+          <span class="icomoon icon-menu-alt-1 text-h3"></span>
+        </button>
       </div>
   
-    </div>
+    </div> 
     <div class="header-right flex items-center gap-5">
       <div class="notification flex">
         <span class="icomoon icon-bell text-2xl"></span>
       </div>
       <div class="profile">
         <div class="profile-avt overflow-hidden">
-          <img class="rounded-full h-16 w-16" src="{{ asset('images/user.jpg') }}" alt="User Avatar">
+          <button class="flex"><img class="rounded-full h-16 w-16" src="{{ asset('images/user.jpg') }}" alt="User Avatar"></button>
         </div>
       </div>
     </div>
