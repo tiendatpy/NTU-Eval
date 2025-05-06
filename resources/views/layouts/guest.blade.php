fd56f44bd7478c37ca2d53b0c4db78ab13ff849d
tiendatpy <datn71356@gmail.com>
2025-05-06 09:53:31 +0700

    fix view login and redirect when logout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,11 +17,11 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <a class="text-5xl flex" href="">
+                <a class="text-6xl flex" href="">
                     <span class="icomoon icon-logo"></span>
                 </a>
             </div>
-            <div class="w-full sm:max-w-md px-10 py-8 bg-white mt-5 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md px-10 py-8 bg-white mt-10 custom-box-shadow overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
