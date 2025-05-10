8962767d2a5cd319e1afd206e4d0b84ed57d98a0
tiendatpy <datn71356@gmail.com>
2025-05-10 22:30:03 +0700

    fix some ui

--- resources/views/globals/header.blade.php
+++ resources/views/globals/header.blade.php
@@ -1,18 +1,18 @@
 <header class="header bg-white h-32 fixed top-0 left-0 right-0 z-50">
   <div class="px-10 flex h-full justify-between items-center">
-    <div class="header-left md:flex md:items-center md:gap-10 xl:gap-16">
+    <div class="header-left md:flex md:items-center md:gap-10 xl:gap-12">
       <div class="logo down_md:hidden">
         <a class="text-[36px] flex" href="{{ route('dashboard') }}">
-          <span class="icomoon icon-logo"></span>
+          <span class="icomoon icon-logo text-states-500"></span>
         </a>
+      </div>
+      <div class="hamburger-menu ">
+        <button class="flex">
+          <span class="icomoon icon-menu-alt-1 text-h3 text-states-500"></span>
+        </button>
       </div>
       <div class="header-search down_xl:hidden flex items-center">
         <input type="text" class="h-[45px] rounded-2xl border-1 w-[402px] px-7 py-6 text-sm" placeholder="Search">
-      </div>
-      <div class="hamburger-menu xl:hidden">
-        <button class="flex">
-          <span class="icomoon icon-menu-alt-1 text-h3"></span>
-        </button>
       </div>
     </div> 
 
