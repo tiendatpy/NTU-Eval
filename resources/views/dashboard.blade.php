725b3cc238d61b77948babf4f05215150ccca763
tiendatpy <datn71356@gmail.com>
2025-06-11 22:58:16 +0700

    fix: Wrap success message in a div for consistent layout in the dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 @section('content')
-    <h1>Bạn đã đăng nhập thành công</h1>
+    <div class="min-h-80p">
+        <h1>Bạn đã đăng nhập thành công</h1>
+    </div>
 @endsection
